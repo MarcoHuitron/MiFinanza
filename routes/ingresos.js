006763f36179997112a5e203d76d7542a162a6ae
Marco Huitron <119384614+MarcoHuitron@users.noreply.github.com>
2025-06-06 10:52:00 -0600

    Update ingresos.js

--- routes/ingresos.js
+++ routes/ingresos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Ingreso = require('../models/ingresos');
+const Ingreso = require('../models/Ingresos');
 
 // GET /api/ingresos/:userId
 // Obtener todos los ingresos de un usuario
